2c7ffe14bdf18f45a6c7467f227813bba0015074
Unknown <abovesnoupi@gmail.com>
2019-02-05 21:38:45 -0500

    Utilitaire pour créer des "side-effects" hyperapp

---
+++ src/utils.js
@@ -0,0 +1,31 @@
+
+import io from 'socket.io-client/dist/socket.io.slim'
+
+const socket = io.connect('http://10.80.108.18:42069');
+
+
+
+// Socket.io baggy (subs & effects builders)
+export const Socket = {
+
+  emit: (props) => ({
+    effect: (props, dispatch) => {
+      socket.emit(props.event, props.data, res => dispatch(props.action, res))
+    },
+    event: props.event,
+    data: props.data,
+    action: props.action
+  }),
+
+  // Listen to location changes
+  on: props => ({
+    effect: (props, dispatch) => {
+      socket.on(props.event, data => {
+        dispatch(props.action, data)
+      })
+      return () => socket.off(props.event)
+    },
+    event: props.event,
+    action: props.action
+  })
+}
