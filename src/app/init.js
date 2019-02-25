250c796839a0c2d9edbf7f88a1200d10cb746da8
Vincent Gagné <abovesnoupi@gmail.com>
2019-02-24 20:43:31 -0500

    vider state initial

--- src/app/init.js
+++ src/app/init.js
@@ -1,33 +1,7 @@
 // Initial state of the app
 export const init = {
-  rooms: [
-    {
-      id:1,
-      title: "Salle 1",
-    },
-    {
-      id:2,
-      title: "Salle 2",
-    }
-  ],
-  messages: [
-    {
-      id:1,
-      user:"Tony 1",
-      content:"Earth's Mightiest Heroes joined forces to take on threats that were too big for any one hero to tackle...",
-    },
-
-    {
-      id:2,
-      user:"Tony 2",
-      content:"The Strategic Homeland Intervention, Enforcement, and Logistics Division...",
-    },
-
-    {
-      id:3,
-      user:"Tony 3",
-      content:"Earth's Mightiest Heroes joined forces to take on threats that were too big for any one hero to tackle...",
-    }
-  ],
+  currentRoom: '',
+  rooms: [],
+  messages: [],
   inputVal: ''
 }
