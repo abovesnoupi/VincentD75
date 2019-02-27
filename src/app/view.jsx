16398af851459371283eb62c7879690a456354d8
Unknown <abovesnoupi@gmail.com>
2019-02-27 11:35:15 -0500

    Fix liens

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -16,9 +16,9 @@ export const view = state => (
       <div class="container">
         <header class="navbar">
           <section class="navbar-section">
-            <a href="..." class="navbar-brand mr-2">Spectre.css</a>
-            <a href="..." class="btn btn-link">Docs</a>
-            <a href="..." class="btn btn-link">GitHub</a>
+            <a href="#" class="navbar-brand mr-2">Spectre.css</a>
+            <a href="#" class="btn btn-link">Docs</a>
+            <a href="#" class="btn btn-link">GitHub</a>
           </section>
           <section class="navbar-section">
             <div class="input-group input-inline">
