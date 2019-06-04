9ede4675c5d9529fb7159900438971d3efb8db40
abovesnoupi <abovesnoupi@gmail.com>
2019-06-04 12:42:35 -0400

    Update index.jsx

--- src/app/components/DocModal/index.jsx
+++ src/app/components/DocModal/index.jsx
@@ -15,6 +15,16 @@ export const DocModal = ({state}) => (
         <div class="content">
           <h1>Lorem ipsum</h1>
           <p>LALALAL</p>
+          <p># Clone project
+          git clone https://github.com/abovesnoupi/VincentD75.git
+
+          cd VincentD75
+
+          npm install                   # Install dependencies
+          npm start                     # Dev server + live reload
+          npm run start:server          # Start websockets server
+          forever start src/server.js   # Run script continuously
+          npm run build                 # Build project<p>
         </div>
       </div>
       <div class="modal-footer">
